9a959bebd7aa3d799cd05970842b42a442823601
RishabhSharma333 <rishabh79874@gmail.com>
2020-11-30 11:45:37 +0530

    added shop and receipt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShopComponent } from './shop/shop.component';
+import { ReceiptComponent } from './shop/receipt/receipt.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShopComponent,
+    ReceiptComponent
   ],
   imports: [
     BrowserModule,
